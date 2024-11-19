b7b1564ed094c4b6b5205f061945d9d41bc4afe7
EdgarAlvarez-ROl <edgaralvares4208@gmail.com>
2024-11-18 23:27:00 -0600

    ADD: dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       colors: {
         'primary': 'rgb(122, 28, 143)',
         'secondary': '#1D778F', 
+        'darkPrimary': 'rgb(200, 152, 243)',
       },
     },
   },
