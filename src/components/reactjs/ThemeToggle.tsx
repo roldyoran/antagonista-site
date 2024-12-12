2fa0340d9482c0faea0e8a0e7fd453104fbcc03e
EdgarAlvarez-ROl <edgaralvares4208@gmail.com>
2024-12-11 20:42:04 -0600

    add: a better navbar

--- src/components/reactjs/ThemeToggle.tsx
+++ src/components/reactjs/ThemeToggle.tsx
@@ -29,16 +29,15 @@ export default function ThemeToggle() {
   return (
     <button
       onClick={toggleTheme}
-      className="mr-6"
     >
       {isDarkMode ? (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="hover:scale-125 transition-all hover:stroke-yellow-400">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="hover:scale-125 p-0 m-0 transition-all hover:stroke-yellow-400">
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
           <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" />
         </svg>
       ) : (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="hover:scale-125 transition-all hover:stroke-indigo-800">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="hover:scale-125 p-0 m-0 transition-all hover:stroke-indigo-800">
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
         </svg>
