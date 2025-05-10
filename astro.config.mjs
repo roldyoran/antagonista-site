e514c1429215173c48acee48c8560b6f37f366b1
roldyoran <edgaralvares4208@gmail.com>
2025-05-10 16:31:29 -0600

    feat(auth): integrate Clerk for user authentication
    Add Clerk middleware, configure dark theme and localization, and update Nav component with SignInButton and UserButton. Include necessary dependencies in package.json and update astro.config.mjs to integrate Clerk.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
+import clerk from "@clerk/astro";
 import vercel from '@astrojs/vercel';
+
+import { dark } from '@clerk/themes'
+import { esMX } from '@clerk/localizations'
 
 // Configuración principal de Astro
 export default defineConfig({
-  // Integraciones con Vue.js y Tailwind CSS
-  integrations: [vue(), tailwind()],
+  // Integraciones con Vue.js y Tailwind CSS y Clerk
+  integrations: [vue(), tailwind(), clerk({
+    appearance: {
+      baseTheme: [dark],
+    },
+    localization: esMX,
+  })],
   // Configuración para renderizado en el servidor
   output: 'server',
   // Adaptador para despliegue en Vercel
