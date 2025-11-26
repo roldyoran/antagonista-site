6554be55fa3a99163b1bd3f96afc5f1fddd59245
roldyoran <edgaralvares4208@gmail.com>
2025-11-25 21:05:45 -0600

    refactor: update .gitignore and astro.config.mjs to remove Vercel references

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
+// import vercel from '@astrojs/vercel';
 
 // Configuración principal de Astro
 export default defineConfig({
   // Integraciones con Vue.js y Tailwind CSS
   integrations: [vue(), tailwind()],
-  // Configuración para renderizado en el servidor
-  output: 'server',
-  // Adaptador para despliegue en Vercel
-  adapter: vercel(),
+  // // Configuración para renderizado en el servidor
+  // output: 'server',
+  // // Adaptador para despliegue en Vercel
+  // adapter: vercel(),
 });
